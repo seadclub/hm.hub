9c2f1e922de834b32528e829bdb50db9db97901d
kenjitheman <btwdesu@gmail.com>
2024-02-17 00:48:44 +0200

    chore: remove code to ensure buildability

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,10 @@
-pub mod bot;
 pub mod db;
 
 use db::create_db;
-use bot::*;
-use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
 
 #[tokio::main]
 async fn main() {
     log::info!("Starting dialogue bot...");
     create_db();
     pretty_env_logger::init();
-
-    let bot = Bot::from_env();
-
-    Dispatcher::builder(
-        bot,
-        Update::filter_message()
-            .enter_dialogue::<Message, InMemStorage<State>, State>()
-            .branch(dptree::case![State::Start].endpoint(start))
-            .branch(dptree::case![State::ReceiveFullName].endpoint(receive_full_name))
-            .branch(dptree::case![State::ReceiveAge { full_name }].endpoint(receive_age))
-            .branch(
-                dptree::case![State::ReceiveLocation { full_name, age }].endpoint(receive_location),
-            ),
-    )
-    .dependencies(dptree::deps![InMemStorage::<State>::new()])
-    .enable_ctrlc_handler()
-    .build()
-    .dispatch()
-    .await;
 }
