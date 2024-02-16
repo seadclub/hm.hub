37cdce1f9f98c0d4ac38e83a03697ae6f8cd949a
kenjitheman <btwdesu@gmail.com>
2024-02-17 00:23:48 +0200

    chore: format code

--- src/db.rs
+++ src/db.rs
@@ -1,36 +1,39 @@
 use rusqlite::Connection;
 
 pub fn create_db() {
-    let conn = Connection::open("your_database.db").expect("Failed to open database");
+    let conn = Connection::open("hh.db").expect("Failed to establish connection to db");
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS users (
-             id INTEGER PRIMARY KEY AUTOINCREMENT
-         )",
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            )",
         [],
-    ).expect("Failed to create users table");
+    )
+    .expect("Failed to create users table");
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS category (
-             id INTEGER PRIMARY KEY AUTOINCREMENT,
-             name TEXT NOT NULL
-         )",
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+            )",
         [],
-    ).expect("Failed to create category table");
+    )
+    .expect("Failed to create category table");
 
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS homework_hub (
-             name TEXT PRIMARY KEY,
-             desc TEXT NOT NULL,
-             deadline DATE,
-             date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-             category_id INTEGER,
-             user_id INTEGER,
-             FOREIGN KEY (category_id) REFERENCES category(id),
-             FOREIGN KEY (user_id) REFERENCES users(id)
-         )",
+        "CREATE TABLE IF NOT EXISTS homework (
+            name TEXT PRIMARY KEY,
+            desc TEXT NOT NULL,
+            deadline DATE,
+            date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            category_id INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY (category_id) REFERENCES category(id),
+            FOREIGN KEY (user_id) REFERENCES users(id)
+            )",
         [],
-    ).expect("Failed to create homework_hub table");
+    )
+    .expect("Failed to create homework_hub table");
 
-    log::info!("created database successfully!");
+    log::info!("DB created successfully!");
 }
