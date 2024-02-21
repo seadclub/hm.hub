c8522a1c96b633485097efc1a46969681b46922f
kenjitheman <btwdesu@gmail.com>
2024-02-21 22:22:53 +0200

    chore: add error handling to create_db fn

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 pub mod db;
 pub mod telegram;
+pub mod utils;
 
 use crate::errors::Result;
 use crate::models::State;
 use db::create_db;
 use dotenv::dotenv;
 use std::sync::Arc;
+use telegram::handler::schema;
 use teloxide::dispatching::dialogue::InMemStorage;
 use teloxide::prelude::*;
-use telegram::handler::schema;
 
 mod errors;
 mod models;
@@ -17,7 +18,7 @@ mod models;
 async fn main() -> Result<()> {
     dotenv().ok();
     log::info!("Starting...");
-    create_db();
+    create_db().expect("Failed to create database at main");
 
     let bot = Bot::new(dotenv::var("TELOXIDE_TOKEN")?);
     let state = Arc::new(State::Start);
