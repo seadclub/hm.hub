56fa30a291fb071bfc47f35558aa1a82900b6a67
DmitroPodolsky <podolskijdmitro1@gmail.com>
2024-02-25 00:13:12 +0200

    feat: add folder commands with <command>.rs

---
+++ src/telegram/commands/general.rs
@@ -0,0 +1,15 @@
+
+use teloxide::{
+    requests::Requester,
+    types::Message,
+    Bot,
+};
+
+pub async fn invalid_state(bot: Bot, msg: Message) -> crate::errors::Result<()> {
+    bot.send_message(
+        msg.chat.id,
+        "I don't understand you. Use /help for the list of available commands.",
+    )
+    .await?;
+    Ok(())
+}
---
+++ src/telegram/commands/start.rs
@@ -0,0 +1,12 @@
+
+use teloxide::{
+    requests::Requester,
+    types::Message,
+    Bot,
+};
+
+pub async fn start(bot: Bot, msg: Message) -> crate::errors::Result<()> {
+    bot.send_message(msg.chat.id, "Welcome to Homework Helper!")
+        .await?;
+    Ok(())
+}
