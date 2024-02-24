43d2377e86336c90388e82854fbb9b3d0385f506
DmitroPodolsky <podolskijdmitro1@gmail.com>
2024-02-25 00:11:46 +0200

    refactor: moved utils to telegram

---
+++ src/telegram/utils.rs
@@ -0,0 +1,68 @@
+use crate::{db::select_all_categories, models::MyDialogue};
+use chrono::NaiveDate;
+use teloxide::types::InlineKeyboardButton;
+
+pub fn check_deadline(deadline: &str) -> bool {
+    let date = NaiveDate::parse_from_str(deadline, "%Y-%m-%d");
+    match date {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+pub fn pages(page: usize) -> Vec<Vec<InlineKeyboardButton>> {
+    let categories = select_all_categories().unwrap();
+    let additional_row: Vec<InlineKeyboardButton> = ["next", "previous"]
+        .iter()
+        .map(|&product| {
+            let callback_data = match product {
+                "next" => format!("next_{}", page + 1),
+                "previous" => format!("previous_{}", page - 1),
+                _ => panic!("Unknown product type"),
+            };
+            InlineKeyboardButton::callback(product.to_string(), callback_data)
+        })
+        .collect();
+
+    if categories.len() <= page * 4 {
+        let mut products = categories[((page - 1) * 4)..]
+            .iter()
+            .map(|product| {
+                vec![InlineKeyboardButton::callback(
+                    product.to_string(),
+                    product.to_string(),
+                )]
+            })
+            .collect::<Vec<_>>();
+        products.push(vec![additional_row[1].clone()]);
+        return products;
+    } else if page == 1 {
+        let mut products = categories[..4]
+            .iter()
+            .map(|product| {
+                vec![InlineKeyboardButton::callback(
+                    product.to_string(),
+                    product.to_string(),
+                )]
+            })
+            .collect::<Vec<_>>();
+        products.push(vec![additional_row[0].clone()]);
+        return products;
+    } else {
+        let mut products = categories[((page - 1) * 4)..(page * 4)]
+            .iter()
+            .map(|product| {
+                vec![InlineKeyboardButton::callback(
+                    product.to_string(),
+                    product.to_string(),
+                )]
+            })
+            .collect::<Vec<_>>();
+        products.push(additional_row);
+        return products;
+    }
+}
+
+pub fn get_telegram_user_id(msg: &MyDialogue) -> String {
+    return msg.chat_id().to_string()
+}
